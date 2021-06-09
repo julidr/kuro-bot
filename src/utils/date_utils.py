1ad99ac621a54d80392ac367335105abd22d2238
Juliana Diaz <juli.milena@hotmail.com>
2021-06-09 01:41:47 -0500

    Add extra param to convert_date_to_str function
    In case that is necessary to specify the format of the date when is a str to convert it into datetime, then added the option to the function.

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -12,14 +12,15 @@ def convert_str_to_date(date: str, date_format: str = '%d/%m') -> datetime:
     return datetime.strptime(date, date_format)
 
 
-def convert_date_to_str(date, date_format: str = '%B %d') -> str:
+def convert_date_to_str(date, date_format: str = '%B %d', date_str_format: str = '%d/%m') -> str:
     """
     Converts a date either str or datetime into an specific String format
 
     :param date: The date to transform into String, can be a str or a datetime
     :param date_format: The string format to transform the given date
+    :param date_str_format: original format of the date in case that is an str
     :return: A string with the respective date information and format
     """
     if type(date) == str:
-        date = convert_str_to_date(date)
+        date = convert_str_to_date(date, date_str_format)
     return datetime.strftime(date, date_format)
