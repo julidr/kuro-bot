4f60e6b6968bb690e045d30af8ae047d79b3e1eb
Juliana Diaz <juli.milena@hotmail.com>
2021-07-06 00:50:19 -0500

    Add DressRepository
    To centralize all retrieve and update operations of Dresses

---
+++ src/karthuria/repository/dress_repository.py
@@ -0,0 +1,34 @@
+import logging
+
+from requests import HTTPError
+
+from karthuria.client import KarthuriaClient
+from karthuria.model.character import Dress
+
+LOG_ID = "DressRepository"
+logging.basicConfig(level=logging.INFO)
+
+
+class DressRepository:
+    """
+    Repository with the information of dresses
+    """
+
+    def __init__(self, client: KarthuriaClient):
+        self.client = client
+
+    def get_dress_by_id(self, dress_id: int) -> Dress:
+        """
+        Search for a Dress information by a given ID.
+        If its not found returns None
+
+        :param dress_id: The id to search the dress
+        :return: The Dress instance found for the given id
+        """
+        dress = None
+        try:
+            dress = self.client.get_dress(dress_id)
+            logging.info('[{0}] - Dress wit id [{1}] retrieved successfully'.format(LOG_ID, dress_id))
+        except HTTPError as error:
+            logging.error("[{0}] - Couldn't retrieve Dress with id [{1}]: {2}".format(LOG_ID, dress_id, error))
+        return dress
