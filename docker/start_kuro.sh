0fc24eedd88cd2caaed91ad4c5b60923a63b9792
Juliana Diaz <juli.milena@hotmail.com>
2021-07-21 01:49:02 -0500

    Add script to build and start kuro-bot service

---
+++ docker/start_kuro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creating Kuro Bot Logging Configs"
+docker config create kuro-bot-logs $KURO_PATH_DATA/kuro-logging.conf
+
+echo "Creating Kuro Bot Secrets"
+docker secret create kuro-bot-secret $KURO_PATH_DATA/kuro-settings.json
+
+echo "Building Kuro Bot Image"
+cd $KURO_PATH
+docker build .
+
+echo "Starting Kuro Bot Service"
+docker service create \
+--name kuro-bot \
+--secret kuro-bot-secret \
+--env SETTINGS_PATH=/run/secrets/kuro-bot-secret \
+--env LOGGING_PATH=/kuro-bot-logs \
+--mount type=bind,source=$KURO_PATH_DATA/data,destination=/kuro-bot \
+kuro-bot:latest
