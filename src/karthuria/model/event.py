0e4645b5cd4b3578010945428c0bec1a3419cf19
Juliana Diaz <juli.milena@hotmail.com>
2021-09-21 20:52:07 -0500

    Fix images urls that were changed to cdn

--- src/karthuria/model/event.py
+++ src/karthuria/model/event.py
@@ -6,7 +6,7 @@ class Event:
     Model class of Events information that can be retrieve from Karthuria API
     """
 
-    EVENT_BANNER_URL = 'https://api.karen.makoo.eu/api/assets/ww/res_en/res/event_permanent/banner/event_banner_{0}.png'
+    EVENT_BANNER_URL = 'https://cdn.starira.xyz/api/assets/ww/res_en/res/event_permanent/banner/event_banner_{0}.png'
 
     def __init__(self, event_id: int, end_date: int = None, name: str = None, start_date: int = None):
         self.event_id = event_id
@@ -30,7 +30,7 @@ class Challenge(Event):
     Model class of Challenge information that can be retrieve from Karthuria API
     """
 
-    CHALLENGE_ICON_URL = 'https://api.karen.makoo.eu/api/assets/jp/res/item_root/large/1_{0}.png'
+    CHALLENGE_ICON_URL = 'https://cdn.starira.xyz/api/assets/jp/res/item_root/large/1_{0}.png'
 
     def __init__(self, challenge_id: int, end_date: int, name: str = None, rarity: int = None, start_date: int = None):
         super().__init__(challenge_id, end_date, name, start_date)
@@ -46,7 +46,7 @@ class Boss(Event):
     Model class of Enemy information that can be retrieve from Karthuria API
     """
 
-    ENEMY_ICON_URL = 'https://api.karen.makoo.eu/api/assets/jp/res/icon/enemy/{0}.png'
+    ENEMY_ICON_URL = 'https://cdn.starira.xyz/api/assets/jp/res/icon/enemy/{0}.png'
 
     def __init__(self, boss_id: int, end_date: int, name: str = None, rarity: int = None, hp_percentage: str = None):
         super().__init__(boss_id, end_date, name, None)
