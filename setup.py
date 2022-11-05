aa34ec936b34fbc5a7fe8f5e6036b40e7e3675ea
Juliana Diaz <juli.milena@hotmail.com>
2022-11-05 00:31:52 -0500

    Open Version 1.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ packages = [
 
 setuptools.setup(
     name='kuro-bot',
-    version='1.2.1',
+    version='1.3.0-RC',
     packages=packages,
     author='Juliana Diaz',
     description='Basic informational Bot of Shoujo Kageki Revue Starlight franchise',
